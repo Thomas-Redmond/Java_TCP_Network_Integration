06af33a12dcd5d414c6918aa36ee673b5ea7781d
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-05-19 17:25:39 +0100

    Basic Program structure for client server TCP started. Used tutorial: https://www.baeldung.com/a-guide-to-java-sockets for the code. Will now begin modifying to allow continusous communication rather than just handling one request

---
+++ TCP_Client.java
@@ -0,0 +1,34 @@
+package TCP_Network;
+
+import java.net.*;
+import java.io.*;
+
+public class TCP_Client{
+  private Socket clientSocket;
+  private PrintWriter out;
+  private BufferedReader in;
+
+  public void startConnection(String ip, int port) throws IOException{
+    clientSocket = new Socket(ip, port);
+    out = new PrintWriter(clientSocket.getOutputStream(), true);
+    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+  }
+
+  public String sendMessage(String msg) throws IOException{
+    out.println(msg);
+    String resp = in.readLine();
+    return resp;
+  }
+
+  public void stopConnection() throws IOException{
+    in.close();
+    out.close();
+    clientSocket.close();
+  }
+
+  public static void main(String[] args){
+    startConnection("127.0.0.1", 6666);
+
+  }
+
+}
